c955735679205c88f5b54f398bd63c4c0ce1798d
13516109 <13516109@std.stei.itb.ac.id>
2019-04-08 21:02:23 +0700

    Added LCD

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -1,26 +1,54 @@
-/* Data */
-int threshold[16] = {3, 34, 63, 91, 94, 120, 146, 167, 170, 192, 214, 233, 237, 255, 275, 291};
-char keypad[16] = {'A', 'B', 'C', 'D', '3', '9', '6', '#', '2', '5', '8', '0', '1', '4', '7', '*'};
+#include <LiquidCrystal.h>
 
+/* Data */
+// KEYPAD DATA
+int threshold[16] = {14, 46, 74, 95, 97, 131, 155, 170, 172, 201, 223, 234, 236, 260, 278, 296};
+char keypad[16] = {'A', 'B', 'C', 'D', '3', '6', '9', '#', '2', '5', '8', '0', '1', '4', '7', '*'};
+
+// ULTRASONIC SENSOR DATA
 long duration;
 float distance;
 
 char selectedKeypad = 0x0;
 char buzzerCount = 0x0;
+int timesAlarmTriggered = 0;
+
+// 7 SEGMENT DISPLAY DATA
+int segmentArray[16] = {252, 96, 218, 242, 102, 182, 190, 224, 254, 246, 238, 62, 156, 122, 158, 142};
+
+// LCD DATA
+const int rs = 7, e = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
 
 /* Pins */
+// ULTRASONIC SENSOR PINS
 const int triggerPin = 9;
 const int echoPin = 10;
+
+// BUZZER PINS
 const int buzzerPin = 11;
+
+// 7 SEGMENT DISPLAY PINS
+const int latchPin = 12;
+const int clockPin = 8;
+const int dataPin = 13;
 
 void operateKeypad();
 void operateBuzzerByTimeFrequency(int miliseconds, int frequency);
 void operateUltrasonicSensor();
+void getString();
 
 void setup() {
   // put your setup code here, to run once:
+  // Setting up ultrasonic sensor pins
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  // Setting up 7 segment display pins
+  pinMode(latchPin,OUTPUT);
+  pinMode(clockPin,OUTPUT);
+  pinMode(dataPin,OUTPUT);
+  // Setting up LCD Display
+  lcd.begin(16,2);
   Serial.begin(9600);
 }
 
@@ -55,20 +83,20 @@ void loop() {
       }
       selectedKeypad = 0x0;
       break;
-    default:
-      selectedKeypad = 0x0;
-      break;
   }
-  
+
+  operateSevenSegment(timesAlarmTriggered);
   delay(500);
 }
 
 void operateKeypad(){
   // KEYPAD
   int value = analogRead(A0);
+
   for (int i = 0; i < 16; i++){
     if (abs(value - threshold[i] <= 1)){
       selectedKeypad = keypad[i];
+      Serial.println(keypad[i]);
       operateBuzzerByTimeFrequency(80, 1000);
       break;
     }
@@ -97,4 +125,24 @@ void operateUltrasonicSensor(){
 
   Serial.print("Distance: ");
   Serial.println(distance);
+}
+
+void operateSevenSegment(int selection){
+  digitalWrite(latchPin,LOW);
+  shiftOut(dataPin,clockPin,MSBFIRST,segmentArray[selection]);
+  digitalWrite(latchPin,HIGH);
+}
+
+void getString(){
+  int i = 0;
+  selectedKeypad = 0x0;
+  lcd.setCursor(0,1);
+  while (i < 5){
+    operateKeypad();
+    if (selectedKeypad == 0x0){
+      i++;
+    } else {
+      lcd.print(selectedKeypad);
+    }
+  }
 }
